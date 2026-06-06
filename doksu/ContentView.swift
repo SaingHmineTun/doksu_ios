99b0da57bf09fb9ea5bfc87c15cfc6de29a18f3c
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2026-06-06 13:02:36 +0630

    Initial Commit

---
+++ doksu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  doksu
+//
+//  Created by Sai Mao on 6/6/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ doksu/doksuApp.swift
@@ -0,0 +1,17 @@
+//
+//  doksuApp.swift
+//  doksu
+//
+//  Created by Sai Mao on 6/6/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct doksuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
